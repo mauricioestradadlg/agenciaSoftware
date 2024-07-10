a9945e596b458b4727de2805fed91b192930d270
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-10 10:48:29 -0600

    inicio actualizado

--- frontend/src/paginas/Inicio.js
+++ frontend/src/paginas/Inicio.js
@@ -5,42 +5,13 @@ export default function Inicio(){
     return (
     <>
     <main>
-            <h1>Esto es el Inicio</h1><br/><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p>
+            <h1>BIENVENIDO A NUESTRA PAGINA WEB OFICIAL!</h1><br/><br/>
+            <img id ="imgInicio" alt ="imagen Inicio" src="./imagenes/webDev.jpg"/><br/>
+            <h1>ENVIANOS UN MENSAJE PARA COTIZAR TU PROYECTO!</h1><br/><br/>
+            <img id ="imgInicio2" alt ="imagen Inicio" src="./imagenes/webDev2.jpg"/><br/>
+            <h1>HACEMOS TUS SUEÑOS REALIDAD!😉</h1><br/><br/>
+            <img id ="imgInicio3" alt ="imagen Inicio" src="./imagenes/webDev3.jpeg"/><br/>
+            
         </main>
     </>);
     
