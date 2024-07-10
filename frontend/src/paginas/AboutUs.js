2ddc0a5afd4138e8688b5f116bff2fd24b7926a9
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-10 11:40:03 -0600

    about us

--- frontend/src/paginas/AboutUs.js
+++ frontend/src/paginas/AboutUs.js
@@ -5,42 +5,36 @@ export default function AboutUs(){
     return (
         <>
         <main>
-            <h1>Sobre Nosotros</h1><br/><br/>
+            <h1>SOBRE NOSOTROS</h1><br/><br/>
+            <img id ="imgInicio4" alt="Imagen aboutUS" src="./imagenes/webDev4.jpg"/>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
+                Somos una agencia de software especializada en desarrollo web para 
+                nuevos negocios y PYMES que requieran una pagina web y asi tener
+                presencia online, la diferencia con nosotros es que si desarrollamos
+                tu pagina web no necesitas pagar web hosting, entregamos tu proyecto 
+                en menos de 1 semana y cotizamos en precios accesibles! Lo cual reduce
+                tus costos operativos, automatizas una actividad importante y ademas 
+                impulsas tu negocio mas rapido! Somos la solucion ideal para tu sitio web
             </p><br/>
+
+            <h1>MISION</h1><br/>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
+                Ayudar a los emprendedores automatizando y mejorando el sitio web
+                de sus negocios
             </p><br/>
+
+            <h1>VISION</h1><br/>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
+                Ser alguien de confianza para los nuevos negocios
+                y empresas establecidas
             </p><br/>
+
+            <h1>VALORES</h1><br/>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
+                Confianza, Lealtad y Honestidad
             </p><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p>
+            
+     
         </main>
         </>);
     
