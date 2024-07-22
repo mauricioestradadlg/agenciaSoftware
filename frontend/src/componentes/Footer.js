91bcdbac218a4d42924674dc184578456cd3204a
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-22 11:38:06 -0600

    social media added

--- frontend/src/componentes/Footer.js
+++ frontend/src/componentes/Footer.js
@@ -10,8 +10,8 @@ export default function Footer(){
         
         <br/>
         <div id ="divFooter">
-            <a href =""><img src ="./imagenes/instagram.png" alt ="instagram" width="50" height="50"/></a><br/>
-            <a href =""><img id="fb" src ="./imagenes/fb.png" alt ="instagram" width="30" height="30"/></a><br/>
+            <a href ="https://www.instagram.com/tecnologiasestrada/"><img src ="./imagenes/instagram.png" alt ="instagram" width="50" height="50"/></a><br/>
+            <a href ="https://www.facebook.com/profile.php?id=61562253226917&is_tour_dismissed"><img id="fb" src ="./imagenes/fb.png" alt ="instagram" width="30" height="30"/></a><br/>
             <Link to = "/blog"><h1 id ="blogFooter">Blog</h1></Link>
         </div>
     </footer>
