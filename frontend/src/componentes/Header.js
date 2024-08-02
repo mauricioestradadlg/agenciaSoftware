45dfe9356bc7db5875e5c2fc67876e378cbf19be
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-08-02 13:02:19 -0600

    cambio logo

--- frontend/src/componentes/Header.js
+++ frontend/src/componentes/Header.js
@@ -4,7 +4,7 @@ function Header(){
     return(
 
         <header>
-            <img alt ="" src ="./imagenes/logo.png" width = "150" height = "150"/>
+            <img alt ="" src ="./imagenes/newLogo2.jpg" width = "150" height = "150"/>
             <h1 id = "h1Index">TECNOLOGIAS ESTRADA</h1>
         </header>
     
